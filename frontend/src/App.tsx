17e99a32eb626aee12d2857080c8400eb9017782
Higor Menezes <higormenezes1996@gmail.com>
2020-10-22 00:01:04 -0300

    request for spotify login

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="http://localhost:3333/session">Login</a>
+    </div>
+  );
+}
+
+export default App;
