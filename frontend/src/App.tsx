ec15b6eced14049cfedf3cfc765887403b9d51f3
Higor Menezes <higormenezes1996@gmail.com>
2020-10-23 22:24:08 -0300

    setup project lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import axios from "axios";
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <a href="http://localhost:3333/session">Login</a>
       <button
-        onClick={() =>
-          axios.get("http://localhost:3333/test", { withCredentials: true })
-        }
+        type="button"
+        onClick={() => {
+          axios.get("http://localhost:3333/test", { withCredentials: true });
+        }}
       >
         Request
       </button>
