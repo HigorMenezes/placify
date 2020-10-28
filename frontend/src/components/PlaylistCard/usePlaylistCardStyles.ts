a5a17e1355b992b87d95bdca1fa94372b36684bc
Higor Menezes <higormenezes1996@gmail.com>
2020-10-27 21:17:31 -0300

    change playlist card style

--- frontend/src/components/PlaylistCard/usePlaylistCardStyles.ts
+++ frontend/src/components/PlaylistCard/usePlaylistCardStyles.ts
@@ -9,12 +9,12 @@ const usePlaylistCardStyles = makeStyles<Theme>((theme) => ({
   },
   cardName: {
     margin: "0 5px",
-    marginTop: 5,
     fontSize: 16,
     fontWeight: theme.fontWeight.semiBold,
     lineHeight: 1,
     color: theme.textColor,
     textTransform: "capitalize",
+    textAlign: "center",
   },
 }));
 
