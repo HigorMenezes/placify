5162aa4fba392381d2448334f2679239a8625cc3
Higor Menezes <higormenezes1996@gmail.com>
2020-10-30 23:24:31 -0300

    create search input controller

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 
 import useSearchStyles from "./useSearchStyles";
 
 function Search(): React.ReactElement {
   const classes = useSearchStyles();
+  const history = useHistory();
+  const location = useLocation();
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const qParam = urlParams.get("q");
+
+    if (qParam) {
+      setSearchInput(qParam);
+    }
+  }, []);
+
+  function handleChangeSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const newSearchValue = event.target.value;
+    setSearchInput(newSearchValue);
+  }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("submit");
+
+    if (searchInput) {
+      const searchParams = new URLSearchParams();
+      searchParams.append("q", searchInput);
+
+      history.push({ search: `?${searchParams.toString()}` });
+    }
   }
 
   return (
     <form className={classes.root} noValidate onSubmit={handleSubmit}>
       <label className={classes.searchContainer} htmlFor="search">
-        <input className={classes.searchInput} name="search" />
+        <input
+          className={classes.searchInput}
+          name="search"
+          value={searchInput}
+          onChange={handleChangeSearch}
+        />
         <button className={classes.searchButton} type="submit">
           <FaSearch size={25} />
         </button>
