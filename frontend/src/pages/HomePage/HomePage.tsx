1611b1b885a8aefd12a074d9fa5711d93c99ad01
Higor Menezes <higormenezes1996@gmail.com>
2020-10-30 23:28:43 -0300

    remove useless import

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import placifyApi from "../../services/placifyApi";
-
 import UserProfileButton from "../../components/UserProfileButton";
 import NewAlbumsList from "../../components/NewAlbumsList";
 import FeaturedPlaylistsList from "../../components/FeaturedPlaylistsList";
